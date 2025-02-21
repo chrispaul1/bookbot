72cbe33241d92dd455b2feeafefdc2479e93d52c
chrispaul1 <chrispaul2374@gmail.com>
2025-02-21 16:53:26 -0600

    readme update

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ def count_char(book_text):
 	return char_count_dict
 
 def convert_dict(dict):
-    dict_list = []
-    for key, val in dict.items():
-        dict_list.append({"char":key,"num":val})
+	dict_list = []
+	for key, val in dict.items():
+		dict_list.append({"char":key,"num":val})
 	return dict_list
 		
 def sort_on(dict):
